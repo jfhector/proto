0ec44a1a9a4bffd0f9c9439f931876ffc4abb7a8
Jean-Francois Hector <jeanfrancois.hector@googlemail.com>
2018-03-21 22:07:44 +0000

    Implemented selector

--- src/components/atoms/Selector.tsx
+++ src/components/atoms/Selector.tsx
@@ -1,42 +1,39 @@
 import * as React from 'react'
+import './Selector.css'
 
 interface Props {
       optionsArray: string[]
       value: string
-      className?: string
       handleSelectorChange?: (newSelection: string) => void
 }
 
-export const Selector: React.StatelessComponent<Props> = (props: Props) => (
-      <select 
-            className={props.className}
-            value={props.value}
-            onChange={
-                  (event) => props.handleSelectorChange(event.target.value)
-            }
-      > 
-            {
-                  props.optionsArray.map(
-                        (arrayElement: string, index: number) => (
-                              <option
-                                    key={arrayElement + String(index)}
-                                    value={arrayElement}
-                              >
-                                    {arrayElement}
-                              </option>
-                        )
-                  )
-            }
-      </select>
-)
+export class Selector extends React.Component<Props, {}> {
+      static defaultProps = {
+            handleSelectorChange: () => {}
+      }
 
-Selector.defaultProps = {
-      handleSelectorChange: () => {}
+      render() {
+            return (
+                  <select 
+                        className='Selector'
+                        value={this.props.value}
+                        onChange={
+                              (event) => this.props.handleSelectorChange(event.target.value)
+                        }
+                  > 
+                        {
+                              this.props.optionsArray.map(
+                                    (arrayElement: string, index: number) => (
+                                          <option
+                                                key={arrayElement + String(index)}
+                                                value={arrayElement}
+                                          >
+                                                {arrayElement}
+                                          </option>
+                                    )
+                              )
+                        }
+                  </select>
+            )
+      }
 }
-
-// export const Selector = styled(Selector_preStyle)`
-//       width: 230px;
-//       height: 30px;
-//       font-size: small;
-//       background-color: ${colors.bgColor.background.fbfbfc}
-// `
