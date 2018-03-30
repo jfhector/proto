018c47ace48358d948f531b881c4d992efb7649f
Jean-Francois Hector <jeanfrancois.hector@googlemail.com>
2018-03-30 15:20:36 +0100

    tidied

--- src/components/molecules/CollapsibleContentBoard/CollapsibleContentBoard.tsx
+++ src/components/molecules/CollapsibleContentBoard/CollapsibleContentBoard.tsx
@@ -3,69 +3,6 @@ import { CollapseButton } from '../..'
 import * as s from './CollapsibleContentBoard.css'
 import classNames = require('classnames')
 
-// const CollapsibleContentBoard: React.StatelessComponent<{
-//       title: string
-//       children: React.ReactNode
-//       expanded?: boolean
-//       rightNode?: React.ReactNode
-//       rightNodeIsSticky?: boolean
-//       rightNodeIsHighlighted?: boolean
-//       handleCollapseButtonClick?: React.MouseEventHandler<HTMLElement>
-// }> = ({
-//       title,
-//       children,
-//       expanded = false,
-//       rightNode = null,
-//       rightNodeIsSticky = false,
-//       rightNodeIsHighlighted = false,
-//       handleCollapseButtonClick = () => { console.log('Button clicked') }
-// }) => (
-//       <div
-//             className={classNames(
-//                   s.CollapsibleContentBoard,
-//                   {
-//                         [s.expanded]: expanded,
-//                         [s.rightNodeIsSticky]: rightNodeIsSticky,
-//                         [s.rightNodeIsHighlighted]: rightNodeIsHighlighted,
-//                   }
-//             )}
-//       >
-//             <div
-//                   className={s.collapseButtonContainer}
-//             >
-//                   <CollapseButton
-//                         expanded={expanded}
-//                         handleClick={handleCollapseButtonClick}
-//                   />
-//             </div>
-
-//             <div 
-//                   className={s.title}
-//             >
-//                   {title}
-//             </div>
-
-//             {expanded &&
-//                   <div
-//                         className={s.rightNodeContainer}
-//                   >
-//                         {!!rightNode && 'Selected measure: '}
-//                         {rightNode}
-//                   </div>
-//             }
-
-//             {expanded &&
-//                   <div
-//                         className={s.childrenContainer}
-//                   >
-//                         {children}
-//                   </div>
-//             }
-//       </div>
-// )
-
-// export { CollapsibleContentBoard }
-
 interface Props {
       title: string
       children: React.ReactNode
