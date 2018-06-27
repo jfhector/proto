b2bd224912e3189355e09e1e36b51c9b53c50705
Jean-Francois Hector <jeanfrancois.hector@googlemail.com>
2018-06-27 10:49:28 +0100

    converted identation in webpack.config

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -4,50 +4,49 @@ const webpack = require('webpack')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
-      entry: ['./src/index'],
-      output: {
-            path: path.join(__dirname, 'dist'),
-            filename: 'bundle.js',
-      },
-      module: {
-            rules: [
-                  {
-                        test: /\.tsx?$/,
-                        use: ['babel-loader', 'awesome-typescript-loader'],
-                        exclude: /node-modules/,
-                  },
-                  {
-                        test: /\.css$/,
-                        loader: 'style-loader',
-                        exclude: /node-modules/,
-                  },
-                  {
-                        test: /\.css$/,
-                        loader: 'typings-for-css-modules-loader', // USING THIS IN PLACE OF css-loader TO GET TYPESCRIPT VALIDATION ACROSS TSX AND CSS
-                        exclude: /node-modules/,
-                        query: {
-                              modules: true,
-                              namedExport: true,
-                              localIdentName: '[local]-[hash:base64:5]'
-                        }
-                  },
-                  {
-                        test: /\.(png|jpg|gif)$/,
-                        loader: 'file-loader',
-                  }
-            ]
-      },
-      resolve: {
-            extensions: ['.ts', '.tsx', '.js', '.jsx']
-      },
-      plugins: [
-            // new webpack.DefinePlugin({
-            //       'process.env.NODE_ENV': '"production"'
-            // }),
-            new HtmlWebpackPlugin({
-                  title: 'Prototype',
-            }), 
-            new webpack.NamedModulesPlugin(),
-      ],
-      devtool: 'eval-source-map'
+    entry: ['./src/index'],
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: 'bundle.js',
+    },
+    module: {
+        rules: [{
+                test: /\.tsx?$/,
+                use: ['babel-loader', 'awesome-typescript-loader'],
+                exclude: /node-modules/,
+            },
+            {
+                test: /\.css$/,
+                loader: 'style-loader',
+                exclude: /node-modules/,
+            },
+            {
+                test: /\.css$/,
+                loader: 'typings-for-css-modules-loader', // USING THIS IN PLACE OF css-loader TO GET TYPESCRIPT VALIDATION ACROSS TSX AND CSS
+                exclude: /node-modules/,
+                query: {
+                    modules: true,
+                    namedExport: true,
+                    localIdentName: '[local]-[hash:base64:5]'
+                }
+            },
+            {
+                test: /\.(png|jpg|gif)$/,
+                loader: 'file-loader',
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['.ts', '.tsx', '.js', '.jsx']
+    },
+    plugins: [
+        // new webpack.DefinePlugin({
+        //       'process.env.NODE_ENV': '"production"'
+        // }),
+        new HtmlWebpackPlugin({
+            title: 'Prototype',
+        }),
+        new webpack.NamedModulesPlugin(),
+    ],
+    devtool: 'eval-source-map'
 }
